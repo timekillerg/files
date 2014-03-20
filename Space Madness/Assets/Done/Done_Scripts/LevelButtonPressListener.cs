1ebf738485d88a3fdc8a4728a3f2a72b74105cbc
Eldar <timekillerg@gmail.com>
2014-03-21 01:12:16 +0300

    Eldar: Add Ice Levels buttons

---
+++ Space Madness/Assets/Done/Done_Scripts/LevelButtonPressListener.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelButtonPressListener : MonoBehaviour {
+
+	
+	public Sprite pressedSprite;
+	private Sprite notPressedSprite;
+	
+	
+	void Start () {
+		notPressedSprite = ((SpriteRenderer)renderer).sprite;
+	}
+	
+	void Update () {
+	}
+	
+	void OnMouseDown()
+	{
+		if (GameObject.Find ("Screen Levels Ice").transform.position.x>=-3 && GameObject.Find ("Screen Levels Ice").transform.position.x <= 3)
+
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;				
+			if (Physics.Raycast (ray, out hit, 100)) {
+				if(hit.collider.gameObject == gameObject)
+				{
+					((SpriteRenderer)renderer).sprite = pressedSprite;	
+				}
+			}
+		}
+	}
+	
+	void OnMouseUp ()
+	{
+		((SpriteRenderer)renderer).sprite = notPressedSprite;
+		if (GameObject.Find ("Screen Levels Ice").transform.position.x>=-3 && GameObject.Find ("Screen Levels Ice").transform.position.x <= 3)
+		if (Input.GetMouseButtonUp(0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;				
+			if (Physics.Raycast (ray, out hit, 100)) {
+				if(hit.collider.gameObject == gameObject)
+				{						
+					MenuItemClicked();
+				}
+			}
+		}
+	}
+	
+	void MenuItemClicked() {
+
+	}
+}
