f2a5a8dd4c3d6b29f5ed1d866d12aa0a1a01e45e
Eldar <timekillerg@gmail.com>
2014-03-23 11:04:12 +0300

    Eldar: Add Level Buttons

--- Space Madness/Assets/Done/Done_Scripts/LevelButtonPressListener.cs
+++ Space Madness/Assets/Done/Done_Scripts/LevelButtonPressListener.cs
@@ -1,53 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LevelButtonPressListener : MonoBehaviour {
+public class LevelButtonPressListener : MonoBehaviour {	
+	public Sprite lockedLevelSprite;
+	public Sprite lockedLevelSpritePressed;
+	public Sprite levelSprite;
+	public Sprite levelSpritePressed;
+	public GameObject star1;
+	public GameObject star2;
+	public GameObject star3;
 
-	
-	public Sprite pressedSprite;
-	private Sprite notPressedSprite;
-	
-	
-	void Start () {
-		notPressedSprite = ((SpriteRenderer)renderer).sprite;
-	}
-	
+		
 	void Update () {
 	}
 	
 	void OnMouseDown()
 	{
-		if (GameObject.Find ("Screen Levels Ice").transform.position.x>=-3 && GameObject.Find ("Screen Levels Ice").transform.position.x <= 3)
 
-		if (Input.GetMouseButtonDown(0)) {
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;				
-			if (Physics.Raycast (ray, out hit, 100)) {
-				if(hit.collider.gameObject == gameObject)
-				{
-					((SpriteRenderer)renderer).sprite = pressedSprite;	
-				}
-			}
-		}
 	}
 	
 	void OnMouseUp ()
 	{
-		((SpriteRenderer)renderer).sprite = notPressedSprite;
-		if (GameObject.Find ("Screen Levels Ice").transform.position.x>=-3 && GameObject.Find ("Screen Levels Ice").transform.position.x <= 3)
-		if (Input.GetMouseButtonUp(0)) {
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;				
-			if (Physics.Raycast (ray, out hit, 100)) {
-				if(hit.collider.gameObject == gameObject)
-				{						
-					MenuItemClicked();
-				}
-			}
-		}
-	}
-	
-	void MenuItemClicked() {
 
 	}
 }
--- Space Madness/Assets/Done/Done_Scripts/LightAnimation.cs
+++ Space Madness/Assets/Done/Done_Scripts/LightAnimation.cs
@@ -5,8 +5,14 @@ using AssemblyCSharp;
 public class LightAnimation : MonoBehaviour {
 
 	public float smooth;
-
 	private float startTime;
+	public Texture meteorLevelsFont;
+	public Texture iceLevelsFont;
+	public Texture downLevelsFont;
+	public Texture sunLevelsFont;
+	public Texture mainFont;
+	public GameObject planMenu;
+	private Texture initialFont;
 		
 	void Update ()
 	{
@@ -17,20 +23,51 @@ public class LightAnimation : MonoBehaviour {
 	{
 		startTime = Time.time;
 	}
+
+	private void ChangeBackground(Texture texture)
+	{
+		if(!(planMenu.renderer.material.mainTexture == texture && light.intensity >=0.45))
+		{
+			if(planMenu.renderer.material.mainTexture != texture)
+				light.intensity = Mathf.Lerp(light.intensity, 0.0f, smooth*10* Time.deltaTime);
+
+			if (light.intensity <= 0.05)
+				planMenu.renderer.material.mainTexture = texture;
+
+			if(planMenu.renderer.material.mainTexture == texture)
+				light.intensity = Mathf.Lerp(light.intensity, 0.5f, smooth*10* Time.deltaTime);
+		}
+	}
 		
 	
 	void IntensityChanging ()
 	{
-		if(AppCore.GetCurrentStatus() == AppCore.Status.LOADING)
+		switch(AppCore.GetCurrentStatus())
 		{
+		case AppCore.Status.LOADING:
 			if(Time.time > (startTime+0.75))
 				light.intensity = Mathf.Lerp(light.intensity, 0.5f, smooth * Time.deltaTime);
-		}
-		else
-		{
+			break;
+		case AppCore.Status.LEVELS_ICE:
+			ChangeBackground(iceLevelsFont);
+			break;
+		case AppCore.Status.LEVELS_DOWN:
+			ChangeBackground(downLevelsFont);
+			break;
+		case AppCore.Status.LEVELS_METEOR:
+			ChangeBackground(meteorLevelsFont);
+			break;
+		case AppCore.Status.LEVELS_SUN:
+			ChangeBackground(sunLevelsFont);
+			break;
+		case AppCore.Status.MAPS:
+			ChangeBackground(mainFont);
+			break;
+		default:
+			planMenu.renderer.material.mainTexture = mainFont;
 			light.intensity = 0.5f;
 			light.color = Color.white;
+			break;
 		}
 	}
-
 }
