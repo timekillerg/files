1687639eaa18fa031a62076133221dcd0ba9497f
Eldar <timekillerg@gmail.com>
2014-06-12 00:30:25 +0300

    Eldar: Add all 7 bonus Appear Effects
    Added effects on bonus pick up action.

--- Space Madness/Assets/Done/Done_Scripts/BonusPickUpHelper.cs
+++ Space Madness/Assets/Done/Done_Scripts/BonusPickUpHelper.cs
@@ -15,7 +15,7 @@ public class BonusPickUpHelper : MonoBehaviour
         if (other.tag == "Player" && bonusPiskUpEffect != null)
         {
             Vector3 position = other.transform.position;
-            position.y = position.y + 2;
+            position.y = position.y + 0;
             Transform child = Instantiate(bonusPiskUpEffect, position, Quaternion.identity) as Transform;
             Destroy(gameObject);
         }
